b1adb4f7e65bb60474ad2de042bc5cd4347948db
Nasrinbegum12 <117616066+Nasrinbegum12@users.noreply.github.com>
2023-11-09 10:03:19 +0530

    DuplicateRemove

---
+++ Basic Programming/DuplicateRemove.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,k;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                for ( int k = j; k<n-1;k++)
+                {
+                    a[k]=a[k+1];
+                   
+                }
+                 n--;
+                 
+            }
+            
+        }
+    }
+     for(int i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    
+}
